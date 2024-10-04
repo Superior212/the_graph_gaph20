3e35f53bd66624b1351bdf5afb21147417e5ffe4
Samson Aderonmu <143148845+samsonHRC@users.noreply.github.com>
2024-10-04 23:07:55 +0100

    Refactor token handling in handleTransfer function

--- src/graph-20.ts
+++ src/graph-20.ts
@@ -14,34 +14,43 @@ import {
 } from "../generated/schema";
 
 export function handleTransfer(event: TransferEvent): void {
+  // Load the Token entity
   let token = Token.load("1");
   if (!token) {
+    // If the Token entity doesn't exist, create it with initial values
     token = new Token("1");
-    token.name = "ApeReward";
-    token.symbol = "APR";
+    token.name = "GRAPH20";
+    token.symbol = "GRP";
     token.decimals = 18;
     token.totalSupply = BigInt.fromI32(0);
   }
   token.save();
 
-  let fromAccount = Account.load(event.params.from.toHex());
+  // Handle the 'from' account
+  let fromAccount = Account.load(event.params.from.toHexString());
   if (!fromAccount) {
-    fromAccount = new Account(event.params.from.toHex());
+    // If the 'from' account doesn't exist, create it
+    fromAccount = new Account(event.params.from.toHexString());
     fromAccount.balance = BigInt.fromI32(0);
   }
+  // Decrease the balance of the 'from' account
   fromAccount.balance = fromAccount.balance.minus(event.params.value);
   fromAccount.save();
 
-  let toAccount = Account.load(event.params.to.toHex());
+  // Handle the 'to' account
+  let toAccount = Account.load(event.params.to.toHexString());
   if (!toAccount) {
-    toAccount = new Account(event.params.to.toHex());
+    // If the 'to' account doesn't exist, create it
+    toAccount = new Account(event.params.to.toHexString());
     toAccount.balance = BigInt.fromI32(0);
   }
+  // Increase the balance of the 'to' account
   toAccount.balance = toAccount.balance.plus(event.params.value);
   toAccount.save();
 
+  // Create a new Transfer entity to record this event
   let transfer = new Transfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+    event.transaction.hash.toHexString() + "-" + event.logIndex.toString()
   );
   transfer.from = fromAccount.id;
   transfer.to = toAccount.id;
@@ -52,11 +61,12 @@ export function handleTransfer(event: TransferEvent): void {
 }
 
 export function handleApproval(event: ApprovalEvent): void {
+  // Create a new Approval entity to record this event
   let approval = new Approval(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+    event.transaction.hash.toHexString() + "-" + event.logIndex.toString()
   );
-  approval.owner = event.params.owner.toHex();
-  approval.spender = event.params.spender.toHex();
+  approval.owner = event.params.owner.toHexString();
+  approval.spender = event.params.spender.toHexString();
   approval.value = event.params.value;
   approval.timestamp = event.block.timestamp;
   approval.block = event.block.number;
@@ -66,8 +76,9 @@ export function handleApproval(event: ApprovalEvent): void {
 export function handleOwnershipTransferred(
   event: OwnershipTransferredEvent
 ): void {
+  // Create a new OwnershipTransferred entity to record this event
   let ownershipTransferred = new OwnershipTransferred(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+    event.transaction.hash.toHexString() + "-" + event.logIndex.toString()
   );
   ownershipTransferred.previousOwner = event.params.previousOwner;
   ownershipTransferred.newOwner = event.params.newOwner;
